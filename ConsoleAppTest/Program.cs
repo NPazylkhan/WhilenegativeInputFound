2b615bd704a312fbd264e3b71db4be8af3b736c4
Nurmakhan Pazylkhan <nurmakhan_pazylkhan@mail.ru>
2022-06-24 16:07:26 +0600

    Add project files.

---
+++ ConsoleAppTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = { 1, 2, 3, 4, -5, 6, -7 };
+            bool negativeInputFound = false;
+            int i = 0;
+            while (!negativeInputFound)
+            {
+                if (input[i] < 0)
+                {
+                    negativeInputFound = true;
+                }
+                i++;
+            }
+
+            Console.WriteLine(input[(i == 0 ? 0 : --i)]);
+            Console.ReadKey();
+        }
+    }
+}
